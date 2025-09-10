25b83af3481175fc05398b843be69c00c67cd21a
uvvpavel <paveluvarov@xmos.com>
2025-09-10 09:49:07 +0100

    code cleanup

--- lib_sw_dac/src/standard_fidelity/software_dac_sf.c
+++ lib_sw_dac/src/standard_fidelity/software_dac_sf.c
@@ -1,16 +1,13 @@
 // Copyright 2024-2025 XMOS LIMITED.
 // This Software is subject to the terms of the XMOS Public Licence: Version 1.
-#include <xs1.h>
 #include <stdint.h>
-#include <stdio.h>
 #include <string.h>
-#include <assert.h>
+#include <xcore/assert.h>
 #include <xcore/port.h>
 #include <xcore/clock.h>
 #include <xcore/channel.h>
 #include <xcore/parallel.h>
 #include "software_dac_sf.h"
-#include <xcore/port.h>
 #include "sdac_sf.h"
 #include "filter_banks.h"
 #include "sw_dac_conf_default.h"
@@ -31,9 +28,6 @@ static uint64_t mkmsk_long(int val) {
     }
     return (1ULL << val) - 1;
 }
-
-
-#include <stdio.h>
 
 void sw_dac_sf_init(sw_dac_sf_t *sd,
                     port_t dac_ports[2], xclock_t clk,
@@ -67,7 +61,7 @@ void sw_dac_sf_init(sw_dac_sf_t *sd,
         sd->pre_distort_pwm_comp_history[i] = &sd->pre_distort_pwm_comp_history_[i][2];
         sd->pre_distort_flat_comp_history[i] = &sd->pre_distort_flat_comp_history_[i][1];
     }
-    assert(pwm_levels <= PWM_MAX_LEN);
+    xassert(pwm_levels <= PWM_MAX_LEN);
     sd->clock_block = clk;
     memcpy(&sd->out_ports[0], dac_ports, 2*sizeof(int));
 
@@ -101,7 +95,6 @@ void sw_dac_sf_init(sw_dac_sf_t *sd,
             uint64_t mask_reversed = mask << (2*pwm_max-(i+pwm_max));
             first_64 = mask_reversed | (1 << (2*pwm_max)) | (mask << (1 + 2*pwm_max));
             sd->pwm_lookup[i] = first_64;
-//            printf("%08x\n", (int) sd->pwm_lookup[i]);
         }
     } else {
         sd->pwm_lookup = &sd->pwm_lookup_table[pwm_max];
@@ -111,7 +104,6 @@ void sw_dac_sf_init(sw_dac_sf_t *sd,
             uint64_t mask_reversed = mask << (2*pwm_max-(i+pwm_max));
             first_64 = mask_reversed | (3 << (2*pwm_max)) | (mask << (2 + 2*pwm_max));
             sd->pwm_lookup[i] = first_64;
-//        printf("%08x\n", (int) sd->pwm_lookup[i]);
         }
     }
 }
@@ -119,7 +111,7 @@ void sw_dac_sf_init(sw_dac_sf_t *sd,
 DECLARE_JOB(filter_task,      (sw_dac_sf_t *, chanend_t, chanend_t));
 DECLARE_JOB(sigma_delta_task, (sw_dac_sf_t *, chanend_t));
 
-static inline int filter_x125_64_i16_o32_n16_phased(sw_dac_sf_t *sd, int32_t *output, int ch, int32_t samples[16]) {
+static inline int filter_x125_64_i16_o32_n16_phased(sw_dac_sf_t *sd, int32_t *output, int32_t samples[16]) {
     switch(sd->bank) {
     case 0:
         filter_x125_64_i4_o8_n16_phase_0123489cde(&output[0],  samples,    &filter_banks_125_64_banks[0][0][0]); // 0
@@ -151,11 +143,9 @@ static inline int filter_x125_64_i16_o32_n16_phased(sw_dac_sf_t *sd, int32_t *ou
         filter_shuffle_n24_n8(samples); // shuffle down 16 place
         return 31;
     }
-
-    (void)ch; // TODO remove this argument from function if not needed
 }
 
-static inline int filter_x125_64_i8_o16_n16_phased(sw_dac_sf_t *sd, int32_t *output, int ch, int32_t samples[16]) {
+static inline int filter_x125_64_i8_o16_n16_phased(sw_dac_sf_t *sd, int32_t *output, int32_t samples[16]) {
     int32_t *filter_bank  = &filter_banks_125_64_banks[2*sd->bank+0][0][0];
     int32_t *filter_bank1 = &filter_banks_125_64_banks[2*sd->bank+1][0][0];
     switch(sd->bank) {
@@ -189,11 +179,9 @@ static inline int filter_x125_64_i8_o16_n16_phased(sw_dac_sf_t *sd, int32_t *out
         filter_shuffle_n24_n16(samples); // shuffle down 8 place
         return 15;
     }
-
-    (void)ch; // TODO remove this argument from function if not needed
 }
 
-static inline int filter_x125_64_i4_o8_n16_phased(sw_dac_sf_t *sd, int32_t *output, int ch, int32_t samples[16]) {
+static inline int filter_x125_64_i4_o8_n16_phased(sw_dac_sf_t *sd, int32_t *output, int32_t samples[16]) {
     int32_t *filter_bank = &filter_banks_125_64_banks[sd->bank][0][0];
     switch(sd->bank) {
     case 5:
@@ -222,8 +210,6 @@ static inline int filter_x125_64_i4_o8_n16_phased(sw_dac_sf_t *sd, int32_t *outp
         filter_shuffle_n24_n20(samples); // shuffle down 4 place
         return 8;
     }
-
-    (void)ch; // TODO remove this argument from function if not needed
 }
 
 #if SW_DAC_DC_REMOVAL_TIME_CONSTANT
@@ -247,6 +233,9 @@ static int dc_removal(sw_dac_sf_t *sd, int ch, int sample, int alpha) {
 #define DC_REMOVAL_ALPHA_192 0
 
 static int dc_removal(sw_dac_sf_t *sd, int ch, int sample, int alpha) {
+    (void)sd;
+    (void)ch;
+    (void)alpha;
     return sample;
 }
 
@@ -259,7 +248,7 @@ int filter_x125_4(sw_dac_sf_t *sd, int32_t *output, int ch, int32_t sample) {
     filter_x2_i2_o4_n32(&sd->filter2[ch][12], sd->filter1[ch], &filter_hashed_25_4[0][0][0]);      // 25_4 is incorrectly defined and duplicated
     filter_x2_i4_o8_n16(&sd->filter3[ch][8], sd->filter2[ch], &filter_8x_coefficients[0][0]);
     filter_x2_i8_o16_n16(&sd->filter4[ch][8], sd->filter3[ch], &filter_16x_coefficients[0][0]);
-    return filter_x125_64_i16_o32_n16_phased(sd, output, ch, &sd->filter4[ch][0]);
+    return filter_x125_64_i16_o32_n16_phased(sd, output, &sd->filter4[ch][0]);
 }
 
 // times 125 divide by 8: 96 -> 1500
@@ -274,7 +263,7 @@ int filter_x125_8(sw_dac_sf_t *sd, int32_t *output, int ch, int32_t sample) {
     // Input samples filter2[0..10], Two FIRs (even odd) on [0..7], [1..8], [2..9], [3..10] produce four samples
     // Output samples go into filter3[12..15], so that there are 16 samples in filter3
     filter_x2_i4_o8_n16(&sd->filter3[ch][12], sd->filter2[ch], &filter_16x_coefficients[0][0]);
-    return filter_x125_64_i8_o16_n16_phased(sd, output, ch, &sd->filter3[ch][0]);
+    return filter_x125_64_i8_o16_n16_phased(sd, output, &sd->filter3[ch][0]);
 }
 
 // times 125 divide by 16: 192 -> 1500
@@ -286,7 +275,7 @@ int filter_x125_16(sw_dac_sf_t *sd, int32_t *output, int ch, int32_t sample) {
     // Input samples filter1[0..8], Two FIRs (even odd) on [0..7] produce two samples, two FIRs (even odd) on [1..8] produce two more samples
     // Output samples go into filter2[12..15], so that there are 16 samples in filter2
     filter_x2_i2_o4_n16(&sd->filter2[ch][12], sd->filter1[ch], &filter_16x_coefficients[0][0]);
-    int n = filter_x125_64_i4_o8_n16_phased(sd, output, ch, &sd->filter2[ch][0]);
+    int n = filter_x125_64_i4_o8_n16_phased(sd, output, &sd->filter2[ch][0]);
     return n;
 }
 
@@ -326,14 +315,6 @@ void filter_task(sw_dac_sf_t *sd, chanend_t c_in, chanend_t c_out) {
                 (void) filter_x125_16(sd, sd->pre_distort_in[1], 1, dataR);
                 mask = 15;
                 break;
-#if defined(TEST_PRINT)
-            case 1500000:
-                sd->pre_distort_in[0][0] = dataL;
-                sd->pre_distort_in[1][0] = dataR;
-                n = 1;
-                mask = 1;
-                break;
-#endif
             }
             int oindex = SDAC_BUF_L;
             int n_vec = (n + 7) >> 3;
@@ -355,25 +336,10 @@ void filter_task(sw_dac_sf_t *sd, chanend_t c_in, chanend_t c_out) {
                 oindex = SDAC_BUF_R;
             }
             sd->bank = (sd->bank + 1) & mask;
-                    
-#ifdef TEST_PRINT
-            #if 1
-            int debug_level = n; // Set to n for highest
-            for(int k = 0; k < debug_level; k++) {
-                if (k < 1)  printf("%11ld , ", sd->filter0[0][k]); else printf("            , ");
-                if (k < 2)  printf("%11ld , ", sd->filter1[0][k]); else printf("            , ");
-                if (k < 4)  printf("%11ld , ", sd->filter2[0][k]);  else printf("            , ");
-                if (k < 8)  printf("%11ld , ", sd->filter3[0][k]);  else printf("            , ");
-                if (k < 16) printf("%11ld , ", sd->filter4[0][k]);  else printf("            , ");
-                printf("%11ld\n", data[i][SDAC_BUF_L + k]);
-            }
-            #endif
-#endif
             data[i][SDAC_BUF_N] = n;
-#ifndef TEST_PRINT
+
             chanend_out_word(c_out, (int) &data[i][0]);
-#endif
-            i = (i+1) & 3;
+            i = (i + 1) & 3;
     }
     chanend_out_control_token(c_out, 1);
 }
