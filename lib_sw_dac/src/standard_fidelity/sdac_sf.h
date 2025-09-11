8fc7cbda2d0f853c7ad0e130f7aae129bcfef3d3
uvvpavel <paveluvarov@xmos.com>
2025-09-11 12:08:14 +0100

    adding comments for filters, their buffers and internal defines

--- lib_sw_dac/src/standard_fidelity/sdac_sf.h
+++ lib_sw_dac/src/standard_fidelity/sdac_sf.h
@@ -1,5 +1,10 @@
 // Copyright 2025 XMOS LIMITED.
 // This Software is subject to the terms of the XMOS Public Licence: Version 1.
+
+// These are offsets to the sw_dac_sf_t struct
+// used from the singa-delta assembly API
+// Do not modify!
+
 #define SDAC_SD_STATE_L  0
 #define SDAC_SD_STATE_R  1
 
@@ -12,7 +17,9 @@
 #define SDAC_OUT_PORTS_L 5   // Two ports
 #define SDAC_OUT_PORTS_R 6
 
-
+// These are offsets to the array exchanged between
+// the filter thread the sigma-delta thread
+// Do not modify!
 
 #define SDAC_FILTER_MAX  42
 #define SDAC_BUF_N       0
